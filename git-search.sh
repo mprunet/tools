6fb1b5477be7e3b1b68e8892477af411fd621338
Michel Prunet <mprunet@users.noreply.github.com>
2022-02-04 15:29:46 +0100

    Ajout options

--- git-search.sh
+++ git-search.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+CONTINUER=1
+ALL=0
+SORT=0
+while [ $CONTINUER == 1 ] ; do
+   case $1 in
+      --sort)
+         SORT=1
+      ;;
+      --all)
+         ALL=1
+      ;;
+      --)
+         CONTINUER=0
+      ;;
+      *)
+         CONTINUER=0
+         break
+      ;;
+   esac
+   shift
+done
 if [ -z "$1" ] ; then
    SEARCHSTRING='api|key|user|uname|pw|password|pass|email|credential|login|token|secret'
 else
@@ -10,5 +31,20 @@ else
       shift
    done 
 fi
+echo ALL $ALL
+echo SORT $SORT
 echo Search for strings $SEARCHSTRING
-git log --all -i -G"$SEARCHSTRING" -p . | grep '^+' | grep -Ei "$SEARCHSTRING" | sort -u 
+if [ $ALL == 0 ] ; then
+   if [ $SORT == 1 ] ; then
+      git log --all -i -G"$SEARCHSTRING" -p . | grep '^+' | grep -Ei "$SEARCHSTRING" | sort -u 
+   else
+      git log --all -i -G"$SEARCHSTRING" -p . | grep '^+' | grep --color -Ei "$SEARCHSTRING"
+   fi
+else
+   if [ $SORT == 1 ] ; then
+      git log --all -p . | grep -Ei "$SEARCHSTRING" | sort -u 
+   else
+      git log --all -p . | grep --color -Ei "$SEARCHSTRING"
+   fi
+fi
+
